4a536114251ea786fa6a4a9916f0da0d44a73e13
Madruzinho <gui.augusto.pessoal@gmail.com>
2025-10-02 22:22:04 -0300

    Enviando atividade circular

--- Atividade.circular/Atividade/main.cpp
+++ Atividade.circular/Atividade/main.cpp
@@ -1,139 +1,160 @@
 #include <iostream>
-#include <unistd.h>
+#include <windows.h>
 
 using namespace std;
 
-unsigned int microsecond = 1000000;
-
-class Lista
-{
-public:
+class lista {
+    public:
     string nome;
     int tempo;
-    Lista *ant, *prox;
-    Lista **Inserir(Lista **, string nome, int t);
-    Lista **Excluir(Lista **, string nome);
-    void Listar(Lista **);
+    lista* elo;
+    lista** inserir(lista**, string n, int t);
+    lista** excluir(lista**, string n);
+    void listar(lista**);
+    lista* pesquisar(lista**, string n);
+    lista** executar(lista**);
 };
 
-Lista** Lista::Inserir(Lista **LD, string n, int t)
-{
-    Lista *novo = new Lista();
+lista** lista::inserir(lista** r, string n, int t) {
+    lista *novo = new lista();
     novo->nome = n;
     novo->tempo = t;
-    if (LD[0] == LD[1]) // se é o primeiro
-    {
-        LD[0] = novo;
-        LD[1] = novo;
-        LD[0]->ant = LD[1];
-        LD[1]->prox = LD[0];
-    }
-    else
-    {
-        Lista *aux;
-        aux = LD[0];
-        while (aux != LD[1] && novo->nome > aux->nome)
-            aux = aux->prox;
 
-        if (aux->ant == LD[1] && novo->nome < LD[1]->nome)
-        { // checar se é o primeiro, aux-> ant tem q ser LD[1] e o nome tem q < q LD[1]->nome
-            novo->prox = LD[0];
-            LD[0]->ant = novo;
-            LD[0] = novo;
-            LD[0]->ant = LD[1];
+    if(r[0] == NULL) {
+        r[0] = novo;
+        r[1] = novo;
+        r[1]->elo = novo; 
+    } else {
+        lista *ant = NULL, *atual = r[0];
+        while(atual != r[1] && novo->nome > atual->nome) {
+            ant = atual;
+            atual = atual->elo;
         }
-        else if (aux->prox == LD[0])
-        { // ultimo, aux->prox tem q ser LD[0]
-            novo->ant = LD[1];
-            LD[1]->prox = novo;
-            LD[1] = novo;
-            LD[1]->prox = LD[1];
+
+        if (atual == r[0] && novo-> nome < atual->nome) {
+            novo->elo = r[0];
+            r[0] = novo;
+            r[1]->elo = r[0];
+        } else if (atual == r[1] && novo->nome > atual-> nome) {
+            r[1]->elo = novo;
+            r[1] = novo;
+            r[1]->elo = r[0];
+        } else {
+            ant->elo = novo;
+            novo->elo = atual;
         }
-        else
-        { // meio de alguem
-            novo->prox = aux;
-            novo->ant = aux->ant;
-            aux->ant->prox = novo;
-            aux->ant = novo;
-        }
-        return LD;
     }
+    return r;
 };
 
-Lista** Lista::Excluir(Lista **LD, string n)
-{
-    Lista *aux = LD[0];
-    while (aux != LD[1] && aux->nome != n)
-        aux = aux->prox;
+void lista::listar(lista **r) {
+    lista *aux = r[0];
 
-    if (LD[0] == LD[1])
-    { // só tem um na lista
-        LD[1] = LD[0] = NULL;
+    while ( aux != r[1]) {
+        cout << aux-> nome << aux-> tempo << endl;
+        aux = aux->elo;
     }
-
-    else if (aux == LD[0] && aux->nome == LD[0]->nome)
-    { // se é o primeiro
-        LD[0] = LD[0]->prox;
-        LD[0]->ant = LD[1];
-    }
-    else if (aux == LD[1])
-    { // se é o ultimo
-        LD[1] = LD[1]->ant;
-        LD[1]->prox = LD[0];
-    }
-    else
-    { // no meio de dois
-        aux->ant->prox = aux->prox;
-    }
-
-    delete (aux);
-    return LD;
+    cout << aux->nome << aux->tempo;
 };
 
-void Lista::Listar(Lista **LD)
-{
-    Lista *aux = LD[0];
-    while (aux != LD[1])
-    {
-        cout << aux->nome << " " << aux->tempo;
-        aux = aux->prox;
-    }
+lista* lista::pesquisar(lista **r, string n) {
+    lista *aux = r[0];
+    while(aux != r[1] && aux->nome != n) {
+        aux = aux->elo;
+    } if(aux->nome == n)
+        return aux;
+        else 
+        return NULL;
 };
 
-void Menu()
-{
-    cout << "1. Inserir processos" << endl;
-    cout << "2. Exibir processor" << endl;
-    cout << "3. Executar processos" << endl;
-    cout << "4. Excluir processos" << endl;
-    cout << "5. Finalizar programa" << endl;
+lista** lista::excluir(lista **r, string n) {
+    lista *aux = r[0], *ant = NULL;
+    while (aux != r[1] && aux->nome != n) {
+        ant = aux;
+        aux = aux->elo;
+    } if (r[0] == r[1]) {
+        r[0] = r[1] = NULL;
+    } else if (aux == r[0]) {
+        r[0] = r[0]-> elo;
+        r[1]->elo = r[0];
+    } else if (aux == r[1]) {
+        r[1] = ant;
+        r[1]-> elo = r[0];
+    } else {
+        ant->elo = aux->elo;
+    }
+    delete(aux);
+    return r;
+};
+
+lista** lista::executar(lista **r) {
+    unsigned int microsegundo = 1000;
+    lista *aux1 = r[0], *aux2;
+    while(aux1 != NULL) {
+        if (aux1->tempo > 0) {
+            cout << aux1->nome << " " << aux1->tempo << endl;
+            Sleep(microsegundo);
+            aux1->tempo--;
+            aux1 = aux1->elo;
+        } else {
+            aux2 = aux1-> elo;
+            r = excluir(r, aux1->nome);
+            if(r[0] != NULL) 
+                aux1 = aux2;
+            else
+            aux1 = r[0];
+        }
+    }
+    return r;
+};
+
+void menu() {
+    cout << "1 - Inserir processos" << endl;
+    cout << "2 - Exibir processos" << endl;
+    cout << "3 - Executar processos" << endl;
+    cout << "4 - Excluir processo" << endl;
+    cout << "5 - Finalizar programa" << endl;
 }
 
-int main()
-{
-    Lista LD, **L, *resp;
-    L = new Lista *[2];
+int main() {
+    lista obj, **L, *resp;
+    L = new lista*[2];
     L[0] = L[1] = NULL;
     string nome;
-    int tempo;
-    int op;
+    int tempo, op;
 
-    do
-    {
-        Menu();
+    do {
+        menu();
         cin >> op;
         switch (op)
         {
         case 1:
-            cout << "Informe o nome, raça, idade e tipo: ";
-            cin >> nome;
-            cin >> tempo;
+            cin >> nome >> tempo;
+            L = obj.inserir(L, nome, tempo);
+            break;
+        
+        case 2:
+            obj.listar(L);
+            break;
 
-            L = LD.Inserir(L, nome, tempo);
-            cout << "Processo inserido" << endl;
+        case 3:
+            L = obj.executar(L);
+            break;
+            
+        case 4:
+            cin >> nome;
+            resp = obj.pesquisar(L, nome);
+            if(resp != NULL)
+             L = obj.excluir(L , nome) ;
+            else
+              cout << "Não achou";
+            break; 
+        default:
+            cout << "Insira uma opçao valida";
             break;
         }
+        cout << endl;
     } while (op != 5);
 
     return 0;
-}
+} 
